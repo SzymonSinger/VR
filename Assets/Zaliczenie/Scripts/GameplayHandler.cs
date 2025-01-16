c78b419ed7465b382881d3c1361b79d02a1be1e3
Jakub BeesQ Łotysz <jakublotysz@gmail.com>
2025-01-16 19:11:00 +0100

    Added UI and Game Ending Mechanic

--- Assets/Zaliczenie/Scripts/GameplayHandler.cs
+++ Assets/Zaliczenie/Scripts/GameplayHandler.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameplayHandler : MonoBehaviour
 {
+    public GameObject winScreen;
     public GameObject deathScreen;
     public GameObject movement;
     public GameObject teleportation;
     public GameObject turning;
 
-    public TextMeshProUGUI TM;
+    public List<TextMeshProUGUI> restartTexts;
     private SoundManager SM;
 
     private bool ScreenShowed = false;
@@ -38,13 +40,25 @@ public class GameplayHandler : MonoBehaviour
         }
     }
 
+    public void CompleteGame() {
+        winScreen.SetActive(true);
+        movement.SetActive(false);
+        teleportation.SetActive(false);
+        turning.SetActive(false);
+        ScreenShowed = true;
+        StartCoroutine(Countdown(5));
+    }
+
     public IEnumerator Countdown(int seconds)
     {
         if (seconds <= 0)
         {
             RestartGame();
         }
-        yield return TM.text = $"Restart in {seconds}";
+        //yield return
+        foreach (var TM in restartTexts) {
+            TM.text = $"Restart in {seconds}";
+        } 
         yield return new WaitForSeconds(1);
         seconds--;
         StartCoroutine(Countdown(seconds));
---
+++ Assets/Zaliczenie/Scripts/ItemCollectManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemCollectManager : MonoBehaviour {
+    [SerializeField] GameObject doorHandle;
+    public GameplayHandler gameplayHandler;
+
+    // A dictionary to hold which items are required
+    // and whether they have been collected.
+    private Dictionary<RequiredItem, bool> requiredItemsStatus = new Dictionary<RequiredItem, bool>()
+    {
+        { RequiredItem.Money,   false },
+        { RequiredItem.Key,     false },
+        { RequiredItem.Glasses, false },
+    };
+
+    // The baseline text for each item
+    private string moneyText = "- Money";
+    private string keyText = "- Key";
+    private string glassesText = "- Glasses";
+
+    public TextMeshProUGUI doorText;
+
+    private void Start() {
+        UpdateDoorText();
+    }
+
+    public void CollectItem(GameObject collectedObject) {
+        RequiredItem itemToCollect;
+
+        var itemName = collectedObject.name;
+        switch (itemName) {
+            case "Money":
+                itemToCollect = RequiredItem.Money;
+                break;
+            case "Key":
+                itemToCollect = RequiredItem.Key;
+                break;
+            case "Glasses":
+                itemToCollect = RequiredItem.Glasses;
+                break;
+            default:
+                itemToCollect = RequiredItem.Glasses;
+                break;
+        }
+        // If this item is part of our dictionary AND not yet collected
+        if (requiredItemsStatus.ContainsKey(itemToCollect) && requiredItemsStatus[itemToCollect] == false) {
+            requiredItemsStatus[itemToCollect] = true;
+            Debug.Log($"Collected: {itemToCollect}");
+
+            if (collectedObject != null) {
+                Destroy(collectedObject);
+            }
+            UpdateDoorText();
+        }
+        else if (requiredItemsStatus.ContainsKey(itemToCollect) && requiredItemsStatus[itemToCollect]) {
+            Debug.Log($"Item {itemToCollect} is already collected.");
+        }
+        else {
+            Debug.Log($"{itemToCollect} is not a required item.");
+        }
+    }
+
+    /// <summary>
+    /// Rebuild the text to show a strike-through if collected,
+    /// or plain text if not collected.
+    /// </summary>
+    private void UpdateDoorText() {
+        // Build multi-line string
+        string fullText = string.Empty;
+
+        // Money
+        fullText += requiredItemsStatus[RequiredItem.Money]
+            ? $"<s>{moneyText}</s>\n"
+            : $"{moneyText}\n";
+
+        // Key
+        fullText += requiredItemsStatus[RequiredItem.Key]
+            ? $"<s>{keyText}</s>\n"
+            : $"{keyText}\n";
+
+        // Glasses
+        fullText += requiredItemsStatus[RequiredItem.Glasses]
+            ? $"<s>{glassesText}</s>\n"
+            : $"{glassesText}\n";
+
+        AllItemsCollected();
+
+        doorText.text = fullText;
+    }
+
+    /// <summary>
+    /// Checks if all items in our dictionary are marked collected.
+    /// </summary>
+    private bool AllItemsCollected() {
+        foreach (var kvp in requiredItemsStatus) {
+            if (!kvp.Value)
+                return false;
+        }
+        doorHandle.SetActive(true);
+        return true;
+    }
+
+    public void TestGameComplete() {
+        gameplayHandler.CompleteGame();
+    }
+}
+
+public enum RequiredItem {
+    Money,
+    Key,
+    Glasses
+}
